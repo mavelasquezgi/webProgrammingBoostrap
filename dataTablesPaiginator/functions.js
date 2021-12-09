71ff283b17dbcd7a85a27958217b383ee66ee48b
mavelasquezgi <mavelasquezgi@unal.edu.co>
2021-12-08 21:29:09 -0500

    add chart with api data

--- dataTablesPaiginator/functions.js
+++ dataTablesPaiginator/functions.js
@@ -1,46 +1,64 @@
 function openDataGov () {
-    var department = [];
-    var year = [];
-    var age = [];
-    fetch('https://www.datos.gov.co/resource/r39m-rntv.json')
+    let adjudicado = [];
+    let precio_base = [];
+    let mes = [];
+
+    fetch('https://www.datos.gov.co/resource/bqgi-b496.json')
         .then(responseSuccess => responseSuccess.json())
         .then(function(data) {
             console.log(data)
             for (let index = 0; index <= data.length; index++ ) {
                 console.log(data[index])
             }
+            let yes = 0
+            let not = 0
             data.forEach(element => {
-                if (element.codptore != undefined && element.ano != undefined && element.age != undefined ) {
-                    department.push(element.codptore);
-                    year.push(element.ano);
-                    age.push(element.age);
+                if (element.adjudicado != undefined && element.precio_base != undefined && element.fecha_de_publicacion_del != undefined ) {
+                    adjudicado.push(element.adjudicado);
+                    precio_base.push(element.precio_base);
+                    let listSplit = element.fecha_de_publicacion_del.split("-")
+                    mes.push(listSplit[1]);
+                    if (element.adjudicado == "Si") {
+                        yes ++;
+                    } else { not ++;}
                 }
             });
-        
-            var chart1 = {
-                y: year,
-                x: department,
-                name: 'year',
+
+            let chart1 = {
+                y: precio_base,
+                x: adjudicado,
+                name: 'precio_base',
                 type: 'bar'
             }
 
-            var chart2 = {
-                y: age,
-                x: department,
-                name: 'age',
-                type: 'bar'
+            let chart2 = {
+                values: [yes, not],
+                labels: ['Adjudicado', 'No Adjudicado'],
+                name: 'Adjudicado',
+                type: 'pie'
             }
 
-            let dataChats = [chart1,chart2];
+            let dataChats = [chart1];
+            let dataChats2 = [chart2];
 
             let layout = {
                 barmode: 'stack',
                 title : {
-                    text: 'Pruebas PCR en Colombia'
+                    text: 'Valor por procesos secop'
                 },
             };
 
+            let layout2 = {
+                barmode: 'stack',
+                title : {
+                    text: 'Porcentaje Adjudicados'
+                },
+                height: 750,
+                width: 850
+            };
+
             Plotly.newPlot('divChar1', dataChats, layout)
+            Plotly.newPlot('divChar2', dataChats2, layout2)
         });    
 }
 
